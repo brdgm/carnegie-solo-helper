bff7d525890805a4992b46a17a88273a25162e7b
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-11-10 16:20:30 +0100

    fix code smells

--- src/components/structure/CityBadge.vue
+++ src/components/structure/CityBadge.vue
@@ -3,10 +3,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
 import City from '@/services/enum/City'
 import getCityMetadata from '@/util/getCityMetadata'
-import { PropType } from 'vue'
 import getRegionMetadata from '@/util/getRegionMetadata'
 import RegionMetadata from '@/services/RegionMetadata'
 
