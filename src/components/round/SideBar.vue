fcd737ec24b9e66940375cd70e682bb754bafa6a
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-11-13 16:44:43 +0100

    andrews plan

--- src/components/round/SideBar.vue
+++ src/components/round/SideBar.vue
@@ -2,6 +2,14 @@
   <div class="sidebar">    
     <p>{{t('sideBar.round')}} <strong>{{round}}</strong> / 20</p>
 
+    <p>
+      <span>{{t('sideBar.nextAction')}}</span><br/>
+      <span class="nextAction">
+        <span v-if="isAdvancedCard" class="unknownAction">?</span>
+        <AppIcon v-else type="action" :name="currentCard.mainAction" class="icon"/>
+      </span>
+    </p>
+
     <p class="buttons">
       <button class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#sidebarDepartmentShopModal">{{t('sideBar.departments')}}</button>
       <button class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#sidebarTimelineModal">{{t('sideBar.timeline')}}</button>
@@ -25,13 +33,17 @@ import { useStateStore } from '@/store/state';
 import DepartmentShop from '../structure/DepartmentShop.vue';
 import ModalDialog from 'brdgm-commons/src/components/structure/ModalDialog.vue';
 import TimelineSelection from './TimelineSelection.vue';
+import AppIcon from '../structure/AppIcon.vue';
+import Card from '@/services/Card';
+import CardType from '@/services/enum/CardType';
 
 export default defineComponent({
   name: "SideBar",
   components: {
     DepartmentShop,
     ModalDialog,
-    TimelineSelection
+    TimelineSelection,
+    AppIcon
   },
   setup() {
     const { t } = useI18n();
@@ -51,6 +63,12 @@ export default defineComponent({
     },
     availableDepartments() : string [] {
       return [...this.navigationState.departments]
+    },
+    currentCard() : Card {
+      return this.navigationState.cardDeck.currentCard
+    },
+    isAdvancedCard() : boolean {
+      return this.currentCard.cardType == CardType.ADVANCED
     }
   }
 })
@@ -69,6 +87,17 @@ export default defineComponent({
   border-bottom-left-radius: 10px;
   background-color: #fbf8f1;
   filter: drop-shadow(#666 3px 3px 3px);
+
+  .nextAction {
+    .icon {
+      width: 50px;
+    }
+    .unknownAction {
+      font-size: 30px;
+      font-weight: bold;
+      margin-left: 15px;
+    }
+  }
 
   .buttons {
     display: flex;
