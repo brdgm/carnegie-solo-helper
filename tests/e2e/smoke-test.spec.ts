e64ab6c92ebd2b386f2298f7ec103cf75e183892
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-03-22 14:43:04 +0100

    Add Playwright E2E test (#17)

---
+++ tests/e2e/smoke-test.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test'
+
+test('smoke test', async ({ page }) => {
+  await page.goto('/')
+
+  // app home
+  await expect(page.locator('h1')).toHaveText('Carnegie Solo Helper')
+  await page.getByRole('link', { name: 'Play Game' }).click()
+
+  // setup game
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.getByRole('link', { name: 'Next' }).click();
+  await page.getByRole('button', { name: 'Pick Department tile' }).click()
+
+  await page.locator('#departmentShopModal .departmentContent .departmentGroup .department img').first().click()
+  await page.locator('#departmentShopModal').getByRole('button', { name: 'Select Department' }).click()
+  await page.getByRole('button', { name: 'Start Game' }).click()
+
+  // abort game
+  await page.getByRole('button', { name: 'Abort Game' }).click()
+  await page.locator('#endGameModal').getByRole('button', { name: 'Abort Game' }).click()
+
+  // app home
+  await expect(page.locator('h1')).toHaveText('Carnegie Solo Helper')
+})
